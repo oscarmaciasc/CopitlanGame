aa642506677083135f04b2d664c16b2ae835fad9
oscarmaciasc <67499114+oscarmaciasc@users.noreply.github.com>
2021-08-10 15:51:24 -0500

    Back Button Sett up and testing dynamic panels on GameSelection scene

--- Assets/Scripts/GameSelection.cs
+++ Assets/Scripts/GameSelection.cs
@@ -1,20 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSelection : MonoBehaviour
 {
-    public GameObject ConfirmationWindow;
+       public GameObject ConfirmationWindowDelete;
+
+    public GameObject[] Games;
+
+    public string[] Names = new string[3];
 
     // Start is called before the first frame update
     void Start()
     {
-        ConfirmationWindow.SetActive(false);
+        ConfirmationWindowDelete.SetActive(false);
+        for(int i=0; i<3; i++)
+        {
+            if(Names[i] != "")
+            {
+                Games[i].gameObject.SetActive(true);
+            }
+            else{
+                Games[i].gameObject.SetActive(false);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ConfirmationWindowDisplayDelete()
+    {
+        ConfirmationWindowDelete.SetActive(true);
+    }
+
+    public void Delete()
+    {
+
+    }
+
+    public void NoDelete()
+    {
+        ConfirmationWindowDelete.SetActive(false);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
