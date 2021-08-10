44967cc8805d3a445c58a83978706ae30dc7cf89
oscarmaciasc <67499114+oscarmaciasc@users.noreply.github.com>
2021-08-10 13:43:02 -0500

    Testing the dynamic panels in GameSelectionScene

--- Assets/Scripts/GameSelection.cs
+++ Assets/Scripts/GameSelection.cs
@@ -6,12 +6,24 @@ public class GameSelection : MonoBehaviour
 {
     public GameObject ConfirmationWindowDelete;
 
-    
+    public GameObject[] Games;
+
+    public string[] Names = new string[3];
 
     // Start is called before the first frame update
     void Start()
     {
         ConfirmationWindowDelete.SetActive(false);
+        for(int i=0; i<3; i++)
+        {
+            if(Names[i] != "")
+            {
+                Games[i].gameObject.SetActive(true);
+            }
+            else{
+                Games[i].gameObject.SetActive(false);
+            }
+        }
     }
 
     // Update is called once per frame
