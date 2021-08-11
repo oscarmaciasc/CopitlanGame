df311d4ae04ad189769d5ecfe38e447eb955c4b6
oscarmaciasc <67499114+oscarmaciasc@users.noreply.github.com>
2021-08-11 10:24:55 -0500

    Limiting the Input Text Field to 12 characters in CharacterSelection script

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class CharacterSelection : MonoBehaviour
 {
+
+    public InputField nameInputField;
+    public string auxString;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    } 
+
+    }
 
     public void BackToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LimitInputString()
+    {
+        if (nameInputField.text.Length > 12)
+        {
+            /*auxString = nameInputField.text;
+            nameInputField.text.Remove();
+            for(int i=0; i<12; i++)
+            {
+                nameInputField.text += auxString[i].ToString();
+            }*/
+            nameInputField.characterLimit = 12;
+        }
     }
 }
