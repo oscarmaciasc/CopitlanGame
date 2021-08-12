938df9c5a894d946a109635461a8a09678a368ea
oscarmaciasc <67499114+oscarmaciasc@users.noreply.github.com>
2021-08-12 17:33:24 -0500

    FileManager script revision

--- Assets/Scripts/FileManager/FileManager.cs
+++ Assets/Scripts/FileManager/FileManager.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
+using System.IO;
+using static System.Environment;
+using static System.IO.Path;
 
 public class FileManager
 {
-    //Generate the xml 
+    
 }
