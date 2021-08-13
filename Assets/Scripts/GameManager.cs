37d2097da652ff044557a37790ec9628133e8a34
oscarmaciasc <67499114+oscarmaciasc@users.noreply.github.com>
2021-08-13 11:16:50 -0500

    Assigning key functionallity (0-9) & numpad keys
    Changes in GameManager script sample scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -53,6 +53,67 @@ public class GameManager : MonoBehaviour
             //
             Debug.Log("Mapa");
         }
+
+        if(Input.GetKeyDown(KeyCode.Alpha0)||Input.GetKeyDown("[0]"))
+        {
+            //
+            Debug.Log("0");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha1)||Input.GetKeyDown("[1]"))
+        {
+            //
+            Debug.Log("1");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2)||Input.GetKeyDown("[2]"))
+        {
+            //
+            Debug.Log("2");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha3)||Input.GetKeyDown("[3]"))
+        {
+            //
+            Debug.Log("3");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha4)||Input.GetKeyDown("[4]"))
+        {
+            //
+            Debug.Log("4");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha5)||Input.GetKeyDown("[5]"))
+        {
+            //
+            Debug.Log("5");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha6)||Input.GetKeyDown("[6]"))
+        {
+            //
+            Debug.Log("6");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha7)||Input.GetKeyDown("[7]"))
+        {
+            //
+            Debug.Log("7");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha8)||Input.GetKeyDown("[8]"))
+        {
+            //
+            Debug.Log("8");
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha9)||Input.GetKeyDown("[9]"))
+        {
+            //
+            Debug.Log("9");
+        }
+
     }
 
     public void SaveData()
