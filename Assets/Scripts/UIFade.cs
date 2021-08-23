775ad155951577784859398816cf5f9939e622b4
oscarmaciasc <67499114+oscarmaciasc@users.noreply.github.com>
2021-08-23 14:16:11 -0500

    Fading between scenes working nice

--- Assets/Scripts/UIFade.cs
+++ Assets/Scripts/UIFade.cs
@@ -5,44 +5,53 @@ using UnityEngine.UI;
 
 public class UIFade : MonoBehaviour
 {
+
     // We attached this script to the canvas and not to the image itself because
     // we want to turn the image on and off and if we attached the script to the
     // image, the script will turn off too.
 
-
     public static UIFade instance;
-    // Reference to the image
+
     public Image fadeScreen;
-    private float fadeSpeed = 1f;
+    public float fadeSpeed;
+
     public bool shouldFadeToBlack;
     public bool shouldFadeFromBlack;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
         instance = this;
         DontDestroyOnLoad(gameObject);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(shouldFadeToBlack)
-        {
-            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
 
-            if(fadeScreen.color.a == 1f)
+        if (shouldFadeToBlack)
+        {
+            Debug.Log("Cambiando a Negro");
+            Debug.Log("a: " + fadeScreen.color.a);
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.fixedDeltaTime));
+
+            if (fadeScreen.color.a == 1f)
             {
+                Debug.Log("Ya cambie a Negro");
                 shouldFadeToBlack = false;
             }
         }
-        
-        if(shouldFadeFromBlack)
-        {
-            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
 
-            if(fadeScreen.color.a == 0f)
+        if (shouldFadeFromBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.fixedDeltaTime));
+            Debug.Log("Cambiando a Transparente");
+            Debug.Log("a: " + fadeScreen.color.a);
+
+            if (fadeScreen.color.a == 0f)
             {
+                Debug.Log("Ya cambie a Transparente");
                 shouldFadeFromBlack = false;
             }
         }
@@ -52,6 +61,7 @@ public class UIFade : MonoBehaviour
     {
         shouldFadeToBlack = true;
         shouldFadeFromBlack = false;
+
     }
 
     public void FadeFromBlack()
@@ -60,3 +70,4 @@ public class UIFade : MonoBehaviour
         shouldFadeFromBlack = true;
     }
 }
+
