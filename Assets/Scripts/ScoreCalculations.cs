08ba01ccdb65e8f1390aa91894a4298c94eab7b7
oscarmaciasc <67499114+oscarmaciasc@users.noreply.github.com>
2021-09-14 13:25:32 -0500

    Fixing exceptions in tutorial playing a partiture

---
+++ Assets/Scripts/ScoreCalculations.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculations : MonoBehaviour
+{
+
+    // GLOSSARY:
+
+    // singleHabitantHappiness = porcentaje de felicidad de habitante unico
+    // goodNotes = notas de la partitura interpretadas correctamente
+    // totalNotes = notas totales de la partitura
+
+    private float singleHabitantHappiness;
+    private int goodNotes;
+    private int totalNotes;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
