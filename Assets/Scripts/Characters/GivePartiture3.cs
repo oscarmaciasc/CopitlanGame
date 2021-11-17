2c58da5b66875fb560a9637b9ab479134721f368
Oscar Alejandro Macias Castillo <oscarmaciasprof@gmail.com>
2021-11-16 22:24:05 -0600

    all NPC's distributed

---
+++ Assets/Scripts/Characters/GivePartiture3.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GivePartiture3 : MonoBehaviour
+{
+    public static GivePartiture3 instance;
+    public bool conversationFinished = false;
+    public bool canCheck = true;
+    private string[] lines = { "Buen dia, espero te haya servido el regalo" };
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameData gameData = new GameData();
+        gameData = XmlManager.instance.LoadGame();
+
+        if (gameData.DoesHavePartiture("partiture3"))
+        {
+            this.gameObject.GetComponent<DialogActivator>().lines = lines;
+            canCheck = false;
+        }
+        else
+        {
+            canCheck = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameData gameData = new GameData();
+        gameData = XmlManager.instance.LoadGame();
+        if (gameData.DoesHavePartiture("partiture3"))
+        {
+            this.gameObject.GetComponent<DialogActivator>().lines = lines;
+        }
+    }
+
+    public void AddPartiture()
+    {
+        GameData gameData = new GameData();
+        gameData = XmlManager.instance.LoadGame();
+
+        if (conversationFinished && !gameData.DoesHavePartiture("partiture3"))
+        {
+            Debug.Log("Regalando Partitura3");
+            XmlManager.instance.AddPartiture("partiture3");
+        }
+    }
+}
